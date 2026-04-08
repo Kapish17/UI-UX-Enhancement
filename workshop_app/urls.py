f88a94afcb4b68e9abe0c205df45d597e5ce192c
Kapish <kapishh17@gmail.com>
2026-04-09 02:03:25 +0530

    Removed email verification and cleaned URL routes for simplified authentication

--- workshop_app/urls.py
+++ workshop_app/urls.py
@@ -7,16 +7,15 @@ app_name = "workshop"
 
 urlpatterns = [
 
+    # Home
     re_path(r'^$', views.index, name='index'),
 
+    # Authentication
     re_path(r'^register/$', views.user_register, name="register"),
-
-    re_path(r'^activate_user/(?P<key>.+)$', views.activate_user),
-    re_path(r'^activate_user/$', views.activate_user),
-
     re_path(r'^login/$', views.user_login, name="login"),
     re_path(r'^logout/$', views.user_logout, name="logout"),
 
+    # Dashboard pages
     re_path(
         r'^status$',
         views.workshop_status_coordinator,
@@ -29,6 +28,7 @@ urlpatterns = [
         name='workshop_status_instructor'
     ),
 
+    # Workshop actions
     re_path(
         r'^accept_workshop/(?P<workshop_id>\d+)',
         views.accept_workshop,
@@ -42,56 +42,22 @@ urlpatterns = [
     ),
 
     re_path(
-        r'^details/(?P<workshop_id>\d+)$',
-        views.workshop_details,
-        name='workshop_details'
-    ),
-
-    re_path(
-        r'^type_details/(?P<workshop_type_id>\d+)$',
-        views.workshop_type_details,
-        name='workshop_type_details'
-    ),
-
-    re_path(
-        r'^type_tnc/(?P<workshop_type_id>\d+)$',
-        views.workshop_type_tnc,
-        name='workshop_type_tnc'
-    ),
-
-    re_path(
         r'^propose/$',
         views.propose_workshop,
         name='propose_workshop'
     ),
 
-    re_path(
-        r'^add_workshop_type$',
-        views.add_workshop_type,
-        name='add_workshop_type'
-    ),
-
-    re_path(
-        r'^delete_attachment_file/(?P<file_id>\d+)$',
-        views.delete_attachment_file,
-        name='delete_attachment_file'
-    ),
-
+    # Workshop types
     re_path(
         r'^types/$',
         views.workshop_type_list,
         name='workshop_type_list'
     ),
 
+    # Profile
     re_path(
         r'^view_profile/$',
         views.view_own_profile,
         name='view_own_profile'
-    ),
-
-    re_path(
-        r'^view_profile/(?P<user_id>\d+)$',
-        views.view_profile,
-        name='view_profile'
     ),
 ]
--- workshop_portal/views.py
+++ workshop_portal/views.py
@@ -2,15 +2,24 @@
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 # Local Imports
 from cms.models import Page
 
 
 def index(request):
+
+    # If user is logged in → go to statistics dashboard
+    if request.user.is_authenticated:
+        return redirect(reverse("statistics_app:public"))
+
+    # Otherwise follow default CMS behaviour
     page = Page.objects.filter(title=settings.HOME_PAGE_TITLE)
+
     if page.exists():
         redirect_url = reverse("cms:home", args=[page.first().permalink])
     else:
         redirect_url = reverse("workshop:index")
+
     return redirect(redirect_url)
