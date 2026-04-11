be19e5d92ad08d96e156953c7f7501f2ade12608
Kapish <kapishh17@gmail.com>
2026-04-12 00:48:17 +0530

    Fixed FilterForm: added workshop_type field and corrected dropdown choices for workshop and state filters

--- statistics_app/forms.py
+++ statistics_app/forms.py
@@ -6,41 +6,61 @@ from workshop_app.models import states, WorkshopType
 
 
 class FilterForm(forms.Form):
+
     from_date = forms.DateField(
+        required=False,
         widget=forms.DateInput(
             attrs={'type': 'date', 'class': 'form-control'}
         )
     )
+
     to_date = forms.DateField(
+        required=False,
         widget=forms.DateInput(
             attrs={'type': 'date', 'class': 'form-control'}
         )
     )
-    workshop_type = forms.ModelChoiceField(
-        queryset=WorkshopType.objects.all(), required=False,
+
+    workshop_type = forms.ChoiceField(
+        choices=[('', 'Select Workshop')],
+        required=False,
         widget=forms.Select(attrs={'class': 'form-control'})
     )
+
     state = forms.ChoiceField(
-        choices=states, required=False,
+        choices=[('', 'Select State')] + list(states),
+        required=False,
         widget=forms.Select(attrs={'class': 'form-control'})
     )
+
     show_workshops = forms.BooleanField(
-        help_text="Show my workshops only", required=False,
+        help_text="Show my workshops only",
+        required=False
     )
+
     sort = forms.ChoiceField(
         choices=(("date", "Oldest"), ("-date", "Latest")),
         help_text="Sort by",
         widget=forms.Select(attrs={'class': 'form-control'})
     )
+
     def __init__(self, *args, **kwargs):
-        start = kwargs.pop("start") if "start" in kwargs else None
-        end = kwargs.pop("end") if "end" in kwargs else None
-        selected_state = kwargs.pop("state") if "state" in kwargs else None
-        selected_type = kwargs.pop("type") if "type" in kwargs else None
-        show_workshops = (kwargs.pop("show_workshops")
-                            if "show_workshops" in kwargs else None)
-        sort = kwargs.pop("sort") if "sort" in kwargs else None
+
+        start = kwargs.pop("start", None)
+        end = kwargs.pop("end", None)
+        selected_state = kwargs.pop("state", None)
+        selected_type = kwargs.pop("type", None)
+        show_workshops = kwargs.pop("show_workshops", None)
+        sort = kwargs.pop("sort", None)
+
         super(FilterForm, self).__init__(*args, **kwargs)
+
+        # Load workshop choices dynamically
+        self.fields["workshop_type"].choices = [
+            ('', 'Select Workshop')
+        ] + [(w.id, w.name) for w in WorkshopType.objects.all()]
+
+        # Set initial values
         self.fields["from_date"].initial = start
         self.fields["to_date"].initial = end
         self.fields["state"].initial = selected_state
